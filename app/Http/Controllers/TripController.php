15c1aa77fe7a2689ecee34c9e60e8949239cea03
lana-lababidy <lanalaba8@gmail.com>
2025-01-11 11:04:12 +0300

    firstCommit

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -13,125 +13,6 @@ use Illuminate\Support\Facades\Validator;
 
 class TripController extends Controller
 {
-    public function joinTrip(Request $request)
-    {
-        $this->authorize('join-trip');
-
-        $validator = Validator::make($request->all(), [
-            'trip_id' => 'required|integer|exists:trips,id',
-            'source_id' => 'required|integer|exists:cities,id',
-            'destination_id' => 'required|integer|exists:cities,id',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['error' => 'Invalid Parameters'], 401);
-        }
-
-        $trip_id = $request->trip_id;
-
-        $trip = Trip::find($trip_id);
-        if (!$trip) {
-            return response()->json(['message' => 'The Trip Does not Exist'], 404);
-        }
-
-        $user = Auth::user();
-
-        // تحقق إذا كان المستخدم قد انضم بالفعل
-        $existingPassenger = TripPassenger::where('trip_id', $trip_id)
-            ->where('client_id', $user->id)
-            ->first();
-
-        if ($existingPassenger) {
-            return response()->json(['message' => 'You are already a member of this Trip الرحلة'], 400);
-        }
-
-        // إنشاء سجل جديد في TripPassenger
-        TripPassenger::create(attributes: [
-            'client_id' => $user->id,
-            'trip_id' => $trip_id,
-            'source_id' => $request->source_id,
-            'destination_id' => $request->destination_id,
-        ]);
-
-        return response()->json([
-            'message' => 'Trip Joined Successfully',
-            'trip' => $trip
-        ]);
-    }
-
-
-    // public function addTrip(Request $request)
-    // {
-    //         // التحقق من صحة البيانات
-    //         $validated = $request->validate([
-    //             'notes' => 'nullable|string',
-    //             'total_seats' => 'required|integer|min:1',
-    //             'dateTime' => 'required|date',
-    //             'source_id' => 'required|exists:cities,id',
-    //             'destination_id' => 'required|exists:cities,id',
-    //         ]);
-
-    //         $user = Auth::user(); // الحصول على المستخدم المتصل حاليًا
-
-    //         if (!$user) {
-    //             return response()->json(['message' => 'يجب تسجيل الدخول لإضافة رحلة.'], 401);
-    //         }
-
-    //         // الحصول على المسافة بين المصدر والوجهة
-    //         $distance = City::find($request->source_id)
-    //             ->distances()
-    //             ->where('city_id', $request->destination_id)
-    //             ->first();
-
-    //         if (!$distance) {
-    //             return response()->json(['message' => 'لا توجد مسافة بين المدن المحددة.'], 400);
-    //         }
-
-    //         // المسافة بالكيلومتر
-    //         $distanceKm = $distance->pivot->distance_km;
-
-    //         // الحصول على الإعدادات من قاعدة البيانات
-    //         $fuelPricePerLiter = Setting::where('key', 'fuel_price')->value('value');
-    //         $fuelEfficiency = Setting::where('key', 'fuel_efficiency')->value('value');
-    //         $maintenanceCostPerKm = Setting::where('key', 'maintenance_cost')->value('value');
-    //         $driverProfitPerKm = Setting::where('key', 'driver_profit_per_km')->value('value');
-
-    //         // التحقق من وجود الإعدادات
-    //         if (!$fuelPricePerLiter || !$fuelEfficiency || !$maintenanceCostPerKm || !$driverProfitPerKm) {
-    //             return response()->json(['message' => 'إعدادات التكلفة مفقودة.'], 500);
-    //         }
-
-    //         // حساب تكلفة الرحلة
-    //         $fuelRequired = $distanceKm / $fuelEfficiency; // حساب كمية الوقود المطلوبة
-    //         $fuelCost = $fuelRequired * $fuelPricePerLiter; // حساب تكلفة الوقود
-    //         $maintenanceCost = $distanceKm * $maintenanceCostPerKm; // حساب تكلفة الصيانة
-    //         $driverProfit = $distanceKm * $driverProfitPerKm; // حساب ربح السائق
-    //         $totalCost = $fuelCost + $maintenanceCost + $driverProfit; // التكلفة الإجمالية
-
-    //         // إنشاء الرحلة
-    //         $trip = new Trip();
-    //         $trip->plate_number = $validated['plate_number'];
-    //         $trip->notes = $request->notes;
-    //         $trip->price = $totalCost;
-    //         $trip->total_seats = $validated['total_seats'];
-    //         $trip->dateTime = $validated['dateTime'];
-    //         $trip->driver_id = $user->id; // تعيين معرف السائق
-    //         $trip->save();
-
-    //         return response()->json([
-    //             'message' => 'تمت إضافة الرحلة بنجاح!',
-    //             'trip' => $trip
-    //         ]);
-    //     }
-
-    //     /**{
-    //     "plate_number": "ABC123",
-    //     "notes": "This is a test trip",
-    //     "price": 100,
-    //     "total_seats": 4,
-    //     "dateTime": "2024-12-01T15:00:00"
-    // }
-    //      */
     public function createTrip(Request $request)
     {
         $this->authorize('create-trip');
@@ -229,5 +110,23 @@ class TripController extends Controller
             'message' => 'Trip created successfully, waiting for approval.',
             'data' => $trip,
         ]);
+    }
+    
+    public function getTripByUser(Request $request)
+    {
+        if ($request->has('id')) {
+            $trip = Trip::find($request->id);
+
+            if (!$trip) {
+                return response()->json(['message' => 'Trip not found'], 404);
+            }
+
+            return response()->json($trip);
+        } else {
+            // If no ID is provided, fetch all trips for the user
+            $userTrips = Trip::where('user_id', auth()->id())->get();
+
+            return response()->json($userTrips);
+        }
     }
 }
---
+++ app/Http/Controllers/TripPassenger.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trip;
+use Dotenv\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TripPassenger extends Controller
+{
+    public function joinTrip(Request $request)
+    {
+        $this->authorize('join-trip');
+
+        $validator = Validator::make($request->all(), [
+            'trip_id' => 'required|integer|exists:trips,id',
+            'source_id' => 'required|integer|exists:cities,id',
+            'destination_id' => 'required|integer|exists:cities,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => 'Invalid Parameters'], 401);
+        }
+
+        $trip_id = $request->trip_id;
+
+        $trip = Trip::find($trip_id);
+        if (!$trip) {
+            return response()->json(['message' => 'The Trip Does not Exist'], 404);
+        }
+
+        $user = Auth::user();
+
+        // تحقق إذا كان المستخدم قد انضم بالفعل
+        $existingPassenger = TripPassenger::where('trip_id', $trip_id)
+            ->where('client_id', $user->id)
+            ->first();
+
+        if ($existingPassenger) {
+            return response()->json(['message' => 'You are already a member of this Trip الرحلة'], 400);
+        }
+
+        // إنشاء سجل جديد في TripPassenger
+        TripPassenger::create(attributes: [
+            'client_id' => $user->id,
+            'trip_id' => $trip_id,
+            'source_id' => $request->source_id,
+            'destination_id' => $request->destination_id,
+        ]);
+
+        return response()->json([
+            'message' => 'Trip Joined Successfully',
+            'trip' => $trip
+        ]);
+    }
+}
